b674e0ba7d355085701d94c599a58296aa24c2f3
liaolinyan2018 <linyan.lao@gmail.com>
2019-01-26 23:36:28 +0800

    给简历添加留言功能完成

--- js/message.js
+++ js/message.js
@@ -1,3 +1,4 @@
+!function() {
 //初始化AV对象
 var APP_ID = 'e4cOL5tKtv7eDlGdpEtYTlxY-gzGzoHsz';
 var APP_KEY = 'Srt6LLMtTSBk7VSfSrq6cmUA';
@@ -6,32 +7,37 @@ AV.init({
   appKey: APP_KEY
 });
 
+//展示历史留言
 var query = new AV.Query('Message');
 query.find()
   .then(
     function(messages) {
-      console.log(messages)
-      console.log(messages[0].attributes) //留言的内容
-      console.log(messages[1].attributes) //留言的内容
       let array = messages.map((item) => item.attributes)
       array.forEach((item) => {
         let li = document.createElement('li')
-        li.innerText = item.content
+        li.innerText = `${item.name}:${item.content}`
         let messageList = document.querySelector('#messageList')
         messageList.appendChild(li)
       })
     })
-    
+
+//实时保存留言到数据库并展示留言
 let myForm = document.querySelector('#postMessageForm')
 myForm.addEventListener('submit',function(e) {
   e.preventDefault()
   let content= myForm.querySelector('input[name=content]').value
+  let name = myForm.querySelector('input[name=name]').value
   var Message = AV.Object.extend('Message');
   var message = new Message();
   message.save({
+    'name': name,
     'content': content
   }).then(function(object) {
-    alert('留言成功');
-    console.log(object)
+    let li = document.createElement('li')
+    li.innerText = `${object.attributes.name}:${object.attributes.content}`
+    let messageList = document.querySelector('#messageList')
+    messageList.appendChild(li)
+    myForm.querySelector('input[name=content]').value = ''
   })
 })
+}.call()
