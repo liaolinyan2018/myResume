7752b49e5fcfd141e33797bf83b239e1f8831d86
liaolinyan <linyan.lao@gmail.com>
2019-05-04 15:39:50 +0800

    回顾及整理代码

--- js/preloading-animation.js
+++ js/preloading-animation.js
@@ -1,7 +1,7 @@
  /*1.0 loading animation.18-12-2*/
  !function() {
-     var view = document.querySelector('#preloading')
-     // view.style.border = '1px solid red';
-     setTimeout(function(){preloading.classList.remove('active');},1500);
+    // var view = document.querySelector('#preloading')
+    // console.dir(view) // null why???
+    setTimeout( function(){ preloading.classList.remove('active'); },2000 );
  }.call()
    
--- js/subMenu.js
+++ js/subMenu.js
@@ -1,17 +1,14 @@
   /*3.0 加二级菜单18-12-2*/ 
 !function() {
-    var view = document.querySelector('nav.menu')
-    // view.style.border = '1px solid red';
-    let liTags = view.querySelectorAll('ul > li');
-    //console.log(aTags) //aTags是hash
-    for(let i=0;i<liTags.length;i++){
-        liTags[i].onmouseenter = function(x){
-            //liTags不可靠，最好不要用aTags[i]
-            x.currentTarget.classList.add('active');
+    let liTags = document.querySelectorAll('nav > ul > li');  // not array , is a Object
+    for( let i = 0 ; i < liTags.length ; i++ ) {
+        liTags[i].onmouseenter = function(e){
+            // console.dir(e) //这个mouse对象
+            // console.dir(e.currentTarget) //html
+            e.currentTarget.classList.add('active');
         }
-        liTags[i].onmouseleave = function(y){
-            //aTags不可靠，最好不要用aTags[i]
-            y.currentTarget.classList.remove('active');
+        liTags[i].onmouseleave = function(e){
+            e.currentTarget.classList.remove('active');
         }
     }
 }.call() 
